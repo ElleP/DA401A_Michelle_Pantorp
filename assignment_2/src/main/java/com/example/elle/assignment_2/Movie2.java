97a75c2691de3f8193503b80716f665ea54ddb7d
Elle <michelle.pantorp@bredband.net>
2015-09-10 11:40:18 +0200

    testar pusha upp en variant av movie.java eftersom jag inte kan pusha upp movie.java

---
+++ assignment_2/src/main/java/com/example/elle/assignment_2/Movie2.java
@@ -0,0 +1,24 @@
+package com.example.elle.assignment_2;
+
+/**
+ * Created by Elle on 2015-09-10.
+ */
+public class Movie2 {
+        public String title;
+        public String year;
+        public String overview;
+        public int fanart;
+        public int poster;
+
+
+        public Movie2(String title, String year, String overview, int fanart, int poster) {
+            this.title = title;
+            this.year = year;
+            this.overview = overview;
+            this.fanart = fanart;
+            this.poster = poster;
+
+        }
+    }
+
+}
