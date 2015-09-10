f4377e0fe6090a53850254e288cd79dbf042a85f
Elle <michelle.pantorp@bredband.net>
2015-09-10 11:43:53 +0200

    testar pusha movie.java

---
+++ assignment_2/src/main/java/com/example/elle/assignment_2/Movie.java
@@ -0,0 +1,23 @@
+package com.example.elle.assignment_2;
+
+/**
+ * Created by Elle on 2015-09-08.
+ */
+
+public class Movie{
+    public String title;
+    public String year;
+    public String overview;
+    public int fanart;
+    public int poster;
+
+
+    public Movie(String title, String year, String overview, int fanart, int poster) {
+        this.title = title;
+        this.year = year;
+        this.overview = overview;
+        this.fanart = fanart;
+        this.poster = poster;
+
+    }
+}
