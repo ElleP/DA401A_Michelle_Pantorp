e964fcd0b3ef2e8e052d573ba4be19586f8ebeac
Elle <michelle.pantorp@bredband.net>
2015-09-29 16:07:32 +0200

    Initial commit assignment 5

---
+++ assignment_5/src/main/java/com/example/elle/assignment_5/MovieAdapter.java
@@ -0,0 +1,116 @@
+package com.example.elle.assignment_5;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Elle on 2015-09-08.
+ */
+
+public class MovieAdapter extends BaseAdapter {
+
+    //Proporties
+    List<Movie> mMovieList;
+    LayoutInflater mLayoutInflater;
+
+    //Konstruktorn
+    public MovieAdapter(List<Movie> mMovieList, LayoutInflater mLayoutInflater) {
+        this.mMovieList = mMovieList;
+        this.mLayoutInflater = mLayoutInflater;
+    }
+
+    @Override
+    public int getCount() {
+
+        return mMovieList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+
+        return mMovieList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (convertView == null) {
+            convertView = mLayoutInflater.inflate(R.layout.movie_item, parent, false);
+        }
+
+
+        Movie movie = (Movie) getItem(position);
+
+        ImageView mCover = (ImageView) convertView.findViewById(R.id.movie_poster);
+        TextView mTitle = (TextView) convertView.findViewById(R.id.movie_title);
+        TextView mYear = (TextView) convertView.findViewById(R.id.movie_year);
+
+        mTitle.setText(movie.getTitle());
+        mYear.setText(movie.getYear());
+
+        try {
+            URL url = new URL(movie.getCoverUrl());
+            new downloadCoversTask(mCover).execute(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return convertView;
+    }
+
+
+    protected class downloadCoversTask extends AsyncTask<URL, Void, Bitmap> {
+
+        ImageView mCover;
+
+        public downloadCoversTask(ImageView mCover) {
+
+            this.mCover = mCover;
+        }
+
+        protected Bitmap doInBackground(URL... params) {
+            URL url = params[0];
+
+            try {
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    InputStream in = new BufferedInputStream((urlConnection).getInputStream());
+                    return BitmapFactory.decodeStream(in);
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            mCover.setImageBitmap(bitmap);
+        }
+    }
+}
---
+++ assignment_5/src/main/java/com/example/elle/assignment_5/MovieFragment.java
@@ -0,0 +1,188 @@
+package com.example.elle.assignment_5;
+
+import android.app.Fragment;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.ActionMode;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView.MultiChoiceModeListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.GridView;
+import android.widget.ProgressBar;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.elle.assignment_5.Movie.*;
+
+/**
+ * Created by Elle on 2015-09-08.
+ */
+
+public class MovieFragment extends Fragment  {
+
+    ArrayList<Movie> mMoviesList = new ArrayList<>();
+    MovieAdapter mAdapter;
+    ProgressBar mProgressbar;
+    Movie mMovie;
+
+    public MovieFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_movie, container, false);
+
+        mProgressbar = (ProgressBar) view.findViewById(R.id.movie_progress_bar);
+
+        final GridView gridview = (GridView) view.findViewById(R.id.movie_list_view);
+        mAdapter = new MovieAdapter(mMoviesList, getActivity().getLayoutInflater());
+        gridview.setAdapter(mAdapter);
+
+
+        gridview.setChoiceMode(GridView.CHOICE_MODE_MULTIPLE_MODAL);
+        gridview.setMultiChoiceModeListener(new MultiChoiceModeListener() {
+
+
+            public static final String TAG = "context_action_menu";
+
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                MenuInflater inflater = mode.getMenuInflater();
+                inflater.inflate(R.menu.context_menu, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.item_delete:
+                        deleteSelectedItems();
+                        mode.finish(); // Action picked, so close the CAB
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+
+            private void deleteSelectedItems() {
+                Log.i(TAG, "deleteSelectedItems");
+                SparseBooleanArray checked = gridview.getCheckedItemPositions();
+                for (int i = 0; i < checked.size(); i++) {
+                    mMoviesList.remove(i);
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+
+            }
+
+            @Override
+            public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+                mode.setTitle(gridview.getCheckedItemCount() + " selected");
+            }
+        });
+
+
+        try{
+            URL url;
+            url = new URL("https://api-v2launch.trakt.tv/movies/popular?extended=images");
+            downloadMoviesTask dmT = new downloadMoviesTask();
+            dmT.execute(url);
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        return view;
+    }
+
+
+    private class downloadMoviesTask extends AsyncTask<URL, Void, List<Movie>> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressbar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected List<Movie> doInBackground(URL... params) {
+            URL url = params[0];
+            try {
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestProperty("Content-type", "application/json");
+                urlConnection.setRequestProperty ("trakt-api-version", "2");
+                String apiKey = "492a165927bfaff86b3030454939981d4e2d94c50515e15e42f41fbf57481a44";
+                urlConnection.setRequestProperty("trakt-api-key", apiKey);
+                try {
+                    InputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
+                    return createListOfMovies(inputStream);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        private List<Movie> createListOfMovies(InputStream inputStream) throws IOException, JSONException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            StringBuilder sb = new StringBuilder();
+
+            String line;
+            while((line = bufferedReader.readLine()) != null){
+                sb.append(line);
+            }
+            mMovie = new Movie();
+            String s = sb.toString();
+            return mMovie.fromJSON(new JSONArray(s));
+        }
+
+        @Override
+        protected void onPostExecute(List<Movie> movies) {
+            super.onPostExecute(movies);
+            mMoviesList.addAll(movies);
+            mAdapter.notifyDataSetChanged();
+            mProgressbar.setVisibility(View.GONE);
+        }
+    }
+}
